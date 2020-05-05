1f7d8a1f4e706def3105e81aceaf574372b56102
Luca Bernardi <git@lucysan.net>
2020-05-05 18:22:57 +0200

    WP_HOME added

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 Releases with `*-onstart` tag download latest Wordpress on start, the rest have verion baked in from the day of the build.
 
+Volumes:  
+  - Wordpress mount point: `/srv/wordpress`: `-v your/wp/files:/srv/wordpress`
+
 Environment variables:
   - `DB_HOST`: Database host
   - `DB_NAME`: Database name
   - `DB_USER`: Database user
   - `DB_PASS`: Database password
+  - `WP_HOME`: FQDN/IP to use in Wordpress (required with `*-onstart` tags)
--- start.sh
+++ start.sh
@@ -9,7 +9,16 @@ sed -i "s/database_name_here/$DB_NAME/" $WP_CONFIG
 sed -i "s/username_here/$DB_USER/" $WP_CONFIG
 sed -i "s/password_here/$DB_PASS/" $WP_CONFIG
 
+if [ ! "x$WP_HOME" = "x" ]; then  # If $WP_HOME not empty
+	echo ":: Provided WP_HOME address is $WP_HOME"
+	echo "define( 'WP_HOME', 'http://$WP_HOME' );" >> $WP_CONFIG
+	echo "define( 'WP_SITEURL', 'http://$WP_HOME' );" >> $WP_CONFIG
+fi
+
 # Start
+echo ":: All done, spinning up."
 php-fpm -y /usr/local/etc/php-fpm.conf -c /usr/local/etc/php/php.ini-production -D
 nginx
-tail -f /var/log/nginx/access.log
+
+echo ":: All aboard! Watching nginx error.log:"
+tail -f /var/log/nginx/error.log
