c75a514176c4fea357acf3192bcb96fbce4cf32b
Luca Bernardi <git@lucysan.net>
2020-05-14 17:20:44 +0200

    New config generation

--- start.sh
+++ start.sh
@@ -26,15 +26,33 @@ chown -R nginx: $WORKDIR
 chmod 777 $WORKDIR
 
 # Config
-echo ":: Injecting database config"
-sed -i "s/localhost/$DB_HOST/" $WP_CONFIG
-sed -i "s/database_name_here/$DB_NAME/" $WP_CONFIG
-sed -i "s/username_here/$DB_USER/" $WP_CONFIG
-sed -i "s/password_here/$DB_PASS/" $WP_CONFIG
+echo ":: Generating config"
+cat > $WP_CONFIG << EOF
+<?php
+define( 'DB_NAME', '$DB_NAME' );
+define( 'DB_USER', '$DB_USER' );
+define( 'DB_PASSWORD', '$DB_PASS' );
+define( 'DB_HOST', '$DB_HOST' );
+define( 'WP_HOME', 'http://$WP_HOME' );
+define( 'WP_SITEURL', 'http://$WP_HOME' );
 
-echo ":: Provided WP_HOME address is $WP_HOME"
-echo "define( 'WP_HOME', 'http://$WP_HOME' );" >> $WP_CONFIG
-echo "define( 'WP_SITEURL', 'http://$WP_HOME' );" >> $WP_CONFIG
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+\$table_prefix = 'wp_';
+define( 'WP_DEBUG', false );
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+require_once ABSPATH . 'wp-settings.php';
+EOF
 
 # Update DB
 if [ ! "x$WP_OLD_HOME" = "x" ]; then
